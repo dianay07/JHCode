c12cf8dd226ae2987cd85dd0dad129938ea338d7
dianay07 <dianay07@naver.com>
2023-04-13 12:12:47 +0900

    [Silver II] Title: 랜선 자르기, Time: 4 ms, Memory: 2288 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -3,59 +3,59 @@
 #include <vector>
 using namespace std;
 
-#define fastio cin.tie(0)->ios::sync_with_stdio(0); cout. tie(0); setvbuf(stdout, nullptr, _IOFBF,BUFSIZ);
+#define fastio cin.tie(0)->ios::sync_with_stdio(0); cout.tie(0); setvbuf(stdout, nullptr, _IOFBF,BUFSIZ);
 
-long long K; // the number of lan lines you already have
-long long N; // Number of LAN cables required
+long long K;	// 이미 가지고 있는 랜선 수
+long long N;	// 필요한 랜선의 갯수
 
 vector<long long> vec;
 
-long long solution (long long left, long long right)
+long long solution(long long left, long long right)
 {
-    long long sum = 0;
-    long long returnValue = 0;
+	long long sum = 0;
+	long long  returnValue = 0;
 
-    while (left <= right)
-    {
-        long long pivot = (left + right) / 2;
-        long long sum = 0;
+	while(left <= right)
+	{
+		long long pivot = (left + right) / 2;
+		long long sum = 0;
 
-        for (long long i = 0; i < K; i++)
-        {
-            sum += vec[i] / pivot;
-        }
+		for (long long i = 0; i < K; i++)
+		{
+			sum += vec[i] / pivot;
+		}
 
-        if (sum >= N)
-        {
-            returnValue = pivot;
-            left = pivot + 1;
-        }
-        else
-        {
-            right = pivot - 1;
-        }
-    }
+		if(sum >= N)
+		{
+			returnValue = pivot;
+			left = pivot + 1;
+		}
+		else
+		{
+			right = pivot - 1;
+		}
+	}
 
-    return returnValue;
+	return returnValue;
 }
 
 int main()
 {
-    fastio;
+	fastio;
 
-    cin >> K >> N;
+	cin >> K >> N;
 
-    for(long long i = 0; i < K; i++)
-    {
-        long long input;
-        cin >> input;
+	for(long long i = 0; i < K; i++)
+	{
+		long long input;
+		cin >> input;
 
-        vec.push_back(input);
-    }
+		vec.push_back(input);
+	}
 
-    sort(vec.begin(), vec.end());
+	sort(vec.begin(), vec.end());
 
-    cout << solution(1, vec[K-1]) << "\n";
+	cout << solution(1, vec[K - 1]) << "\n";
 
-    return 0;
+	return 0;
 }
