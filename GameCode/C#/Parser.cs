e6dc0ddd162296b2e9c67dca0406a77fcbd4e884
dianay07 <dianay07@naver.com>
2023-11-15 20:03:29 +0900

    Update repository

---
+++ GameCode/C#/Parser.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parser : MonoBehaviour
+{
+    // 어떤 종류의 데이터 액셀 읽기
+    public Data[] DataParse(string fileName)
+    {
+        // 데이터 덩어리를 리스트로 생성
+        List<Data> datalist = new List<Data>();
+        TextAsset csvData = Resources.Load<TextAsset>("Data/" + fileName);
+
+        // 띄어쓰기를 기준으로 나누기
+        string[] data = csvData.text.Split(new char[] { '\n' });
+
+        for (int i = 1; i < data.Length; i++)
+        {
+            // 대사같은 거면 , 단위로 나누기
+            //string[] row = data[i].Split(new char[] { ',' });
+
+            // Data 객체 생성 후 내용 설정
+            Data item = new Data();
+            //Data.name = row[0];
+            //Data.price = Double.Parse(row[1]);
+            //Data.id = Int32.Parse(row[3]);
+            //Data.desc = row[4];
+            //Data.itemEffect = float.Parse(row[5]);
+            //Data.effectdesc = row[6];
+
+            datalist.Add(item);
+            GameManager.dbManager.giveinfo.itemlist.Add(item);
+        }
+
+        return itemlist.ToArray();
+    }
+}
+
---
+++ GameCode/C#/UI_IngameWnd.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class UI_IngameWnd : UI_Base
+{
+    [SerializeField] UI_Obj ui_inven;                                
+
+    // 순서대로 담을 스택
+    [SerializeField] Stack<GameObject> PopupUI_Stack = new Stack<GameObject>();
+
+    // 단축키 지정
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.I)) { }
+        else if (Input.GetKeyDown(KeyCode.Q)) { }
+        else if (Input.GetKeyDown(KeyCode.Escape){ }
+
+
+    }
+
+    // UI 닫기
+    public void CloseOrEnd() 
+    {
+        // 1개 이상 UI가 켜져있으면 삭제
+        if (PopupUI_Stack.Count != 0)
+            PopUIObj();
+        // 0개일때 눌렷을경우
+        else              
+        {
+
+        }
+    }
+
+    // 인벤토리 키고 끄기
+    public void inventoryUIOpen()
+    {
+        // 안켜져있을때
+        if (ui_inven.isActiveAndEnabled == false) 
+        {
+            //SwitchActive(ui_inven.transform.parent.transform);
+            //ui_inven.refreshInven();      // UI 새로그리기
+        }
+        else
+        {
+            // 스택안에 모든 UI 삭제
+            foreach (var item in PopupUI_Stack)
+            {
+                Destroy(item);
+            }
+            PopupUI_Stack.Clear();
+            SwitchActive(ui_inven.transform.parent.transform);
+        }
+    }
+
+    // 생성된 UI 오브젝트 추가
+    public GameObject PushUIObj(GameObject obj)
+    {
+        // 켜져있으면
+        PopupUI_Stack.Push(obj);
+
+        return obj;
+    }
+
+    // 맨위 UI 삭제
+    public GameObject PopUIObj()
+    {
+        GameObject destroyObj = PopupUI_Stack.Pop();
+        Destroy(destroyObj);
+
+        return destroyObj;
+    }
+}
