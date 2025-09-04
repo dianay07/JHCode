7308036a2e4e2b1be646b5b083950936b77fd576
dianay07 <dianay07@naver.com>
2025-09-04 18:29:33 +0900

    [level 2] Title: 짝지어 제거하기, Time: 5.98 ms, Memory: 6.45 MB -BaekjoonHub

---
+++ 프로그래머스/2/12973. 짝지어 제거하기/짝지어 제거하기.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+
+int solution(string s)
+{
+    int answer = -1;
+    
+    stack<char> st;
+    
+    for(int i = 0; i < s.length(); i++)
+    {
+        if(st.empty() || st.top() != s[i])
+            st.push(s[i]);
+        else if(st.top() == s[i])
+            st.pop();
+    }
+    
+    if(st.empty() == true)
+        return 1;
+    else
+        return 0;
+   
+}
